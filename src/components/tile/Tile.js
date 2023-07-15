51d75222e135ea2127b026524af6aea9e3f2f090
persiyana <persiyanageorgieva@gmail.com>
2023-07-15 11:12:54 +0300

    sixth task

--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -1,9 +1,16 @@
 import React from "react";
 
-export const Tile = () => {
+export const Tile = ({ personName, description }) => {
   return (
     <div className="tile-container">
-      
+      <p className="tile-title tile">
+        {personName}
+      </p>
+      {Object.values(description).map((value, index) => (
+        <p key={index} className="tile">
+          {value}
+        </p>
+      ))}
     </div>
   );
 };
