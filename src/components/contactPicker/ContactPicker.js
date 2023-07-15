55696df9c59abbd88347fa14503f52d857d2951c
persiyana <persiyanageorgieva@gmail.com>
2023-07-15 11:34:32 +0300

    ninth task

--- src/components/contactPicker/ContactPicker.js
+++ src/components/contactPicker/ContactPicker.js
@@ -1,7 +1,20 @@
 import React from "react";
 
-export const ContactPicker = () => {
+export const ContactPicker = ({name, value, contacts, onChange}) => {
   return (
-    <></>
+    <>
+      <select onChange={onChange} value={value} name={name}>
+      <option value={""} key={-1}>
+        No Contact Selected
+      </option>
+      {contacts.map((contact) => {
+        return (
+          <option value={contact} key={contact}>
+            {contact}
+          </option>
+        );
+      })}
+      </select>
+    </>
   );
 };
